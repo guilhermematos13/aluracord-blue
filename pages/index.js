5d2e71ede2eb36991016e8bef0682cead7519080
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2022-09-28 19:09:35 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,173 @@
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import appConfig from "../config.json";
+
+function GlobalStyle() {
+    return (
+      <style global jsx>{`
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          list-style: none;
+        }
+        body {
+          font-family: 'Open Sans', sans-serif;
+        }
+        /* App fit Height */ 
+        html, body, #__next {
+          min-height: 100vh;
+          display: flex;
+          flex: 1;
+        }
+        #__next {
+          flex: 1;
+        }
+        #__next > * {
+          flex: 1;
+        }
+        /* ./App fit Height */ 
+      `}</style>
+    );
+  }
+
+function Title(props){
+    console.log(props);
+    const Tag = props.tag || 'h1';
+    return(
+        <>
+            <Tag>{props.children}</Tag>
+            <style jsx>{`
+            ${Tag} {
+            color: ${appConfig.theme.colors.primary['500']};
+            font-size: 24px;
+            font-weight: 600;
+            }
+            `}</style>
+        </>
+    );
+}
+
+// function HomePage() {
+//     return ( 
+//     <div>
+//         <GlobalStyle />
+//         <Title tag="h2">Bem vindo de volta!</Title>
+//         <h2>Discord dos amigos - Guizão</h2>
+        
+       
+//         </div>
+//     )
+    
+// }
+  
+// export default HomePage
+
+export default function PaginaInicial() {
+    const username = 'guilhermematos13';
+  
+    return (
+      <>
+        <GlobalStyle />
+        <Box
+          styleSheet={{
+            display: 'flex', alignItems: 'center', justifyContent: 'center',
+            backgroundColor: appConfig.theme.colors.primary[500],
+            backgroundImage: 'url(https://img.freepik.com/free-photo/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-52379.jpg?w=996&t=st=1663977145~exp=1663977745~hmac=8ed87cc17379961b66f7a2db7f3bbb5644f16c2b07000523b02c1cc0bf7d89cd)',
+            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+          }}
+        >
+          <Box
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              flexDirection: {
+                xs: 'column',
+                sm: 'row',
+              },
+              width: '100%', maxWidth: '700px',
+              borderRadius: '5px', padding: '32px', margin: '16px',
+              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+              backgroundColor: appConfig.theme.colors.neutrals[700],
+            }}
+          >
+            {/* Formulário */}
+            <Box
+              as="form"
+              styleSheet={{
+                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+              }}
+            >
+              <Title tag="h2">Boas vindas de volta!</Title>
+              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals['000'] }}>
+                {appConfig.name}
+              </Text>
+  
+              <TextField
+                fullWidth
+                textFieldColors={{
+                  neutral: {
+                    textColor: appConfig.theme.colors.neutrals['000'],
+                    mainColor: appConfig.theme.colors.neutrals[900],
+                    mainColorHighlight: appConfig.theme.colors.primary[500],
+                    backgroundColor: appConfig.theme.colors.neutrals[900],
+                  },
+                }}
+              />
+              <Button
+                type='submit'
+                label='Entrar'
+                fullWidth
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["900"],
+                  mainColor: appConfig.theme.colors.primary[500],
+                  mainColorLight: appConfig.theme.colors.primary[400],
+                  mainColorStrong: appConfig.theme.colors.primary[900],
+                }}
+              />
+            </Box>
+            {/* Formulário */}
+  
+  
+            {/* Photo Area */}
+            <Box
+              styleSheet={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                maxWidth: '200px',
+                padding: '16px',
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                border: '1px solid',
+                borderColor: appConfig.theme.colors.neutrals[999],
+                borderRadius: '10px',
+                flex: 1,
+                minHeight: '240px',
+              }}
+            >
+              <Image
+                styleSheet={{
+                  borderRadius: '50%',
+                  marginBottom: '16px',
+                }}
+                src={`https://github.com/${username}.png`}
+              />
+              <Text
+                variant="body4"
+                styleSheet={{
+                  color: appConfig.theme.colors.neutrals[900],
+                  backgroundColor: appConfig.theme.colors.primary['700'],
+                  padding: '3px 10px',
+                  borderRadius: '1000px'
+                }}
+              >
+                {username}
+              </Text>
+            </Box>
+            {/* Photo Area */}
+          </Box>
+        </Box>
+      </>
+    );
+  }
